7e24c335b874af550ef5a16d11b2d67825891c41
Benjamin <benjamin@MacBook-Pro-de-Benjamin.local>
2021-02-15 17:40:38 +0100

    remove unused font

--- pages/_document.js
+++ pages/_document.js
@@ -9,12 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
