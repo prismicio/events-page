4e009e5c4e3fffbdc1c0ac8b1d3a5f1b2659a29e
Benjamin <benjamin@MacBook-Pro-de-Benjamin.local>
2021-02-16 17:52:38 +0100

    ok

---
+++ pages/api/mailchimp.js
@@ -0,0 +1,35 @@
+import mailchimp from '@mailchimp/mailchimp_marketing';
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+};
+
+mailchimp.setConfig({
+  apiKey: 'c890be2138cdf656f10a127f23530db7-us4',
+  server: 'us4',
+});
+
+export default async function addMember(req, res) {
+  const { listId, email, firstName, lastName } = JSON.parse(req.body);
+  try {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName,
+      },
+    });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+  } catch (err) {
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(err));
+  }
+}
---
+++ pages/api/preview.js
@@ -0,0 +1,39 @@
+import Prismic from 'prismic-javascript';
+
+const REPOSITORY = process.env.PRISMIC_REPOSITORY_NAME;
+const REF_API_URL = `https://${REPOSITORY}.prismic.io/api/v2`;
+const GRAPHQL_API_URL = `https://${REPOSITORY}.prismic.io/graphql`;
+export const API_TOKEN = process.env.PRISMIC_API_TOKEN;
+export const API_LOCALE = process.env.PRISMIC_REPOSITORY_LOCALE;
+
+// Client method to query from the Prismic repo
+const Client = (req = null) =>
+  Prismic.client(REF_API_URL, createClientOptions(req, API_TOKEN));
+
+const createClientOptions = (req = null, prismicAccessToken = null) => {
+  const reqOption = req ? { req } : {};
+  const accessTokenOption = prismicAccessToken
+    ? { accessToken: prismicAccessToken }
+    : {};
+  return {
+    ...reqOption,
+    ...accessTokenOption,
+  };
+};
+
+const Preview = async (req, res) => {
+  const { token: ref, documentId } = req.query;
+  const redirectUrl = await Client(req)
+    .getPreviewResolver(ref, documentId)
+    .resolve(null, '/');
+
+  if (!redirectUrl) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  res.setPreviewData({ ref });
+  res.writeHead(302, { Location: `${redirectUrl}` });
+  res.end();
+};
+
+export default Preview;
