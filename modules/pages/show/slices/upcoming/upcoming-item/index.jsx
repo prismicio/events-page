ea2dea5e52b0234e6607eb148bc7cbe47a5b2003
Benjamin <benjamin@MacBook-Pro-de-Benjamin.local>
2021-02-16 16:32:09 +0100

    wip

--- modules/pages/show/slices/upcoming/upcoming-item/index.jsx
+++ modules/pages/show/slices/upcoming/upcoming-item/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Box, Text, Image, Flex } from '@chakra-ui/react';
+import { format } from 'date-fns';
 import { RichText } from 'prismic-reactjs';
 import htmlSerializer from '@/modules/commons/serializer';
 
 const UpcomingItem = ({ event, icon }) => {
+  const eventDate = format(new Date(event.date), 'dd MMMM yyyy HH:mm');
   return (
     <Box mb="4">
       <RichText render={event.title} htmlSerializer={htmlSerializer} />
       <Flex mt={4} justifyContent="space-between" alignItems="center">
-        <Text variant="date">{event.date}</Text>
+        <Text variant="date">{eventDate}</Text>
         <Image
           src={icon?.url}
           alt={icon?.alt}
