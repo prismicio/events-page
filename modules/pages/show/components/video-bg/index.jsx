a0a0e2d6d99ac82f857d80dfd2fb7757d63f42ec
Benjamin <benjamin@MacBook-Pro-de-Benjamin.local>
2021-02-18 22:49:54 +0100

    add cloudinary

--- modules/pages/show/components/video-bg/index.jsx
+++ modules/pages/show/components/video-bg/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@chakra-ui/react';
+import { Video } from 'cloudinary-react';
 
 const VideoBg = ({ source }) => {
   const ref = useRef();
@@ -18,7 +19,18 @@ const VideoBg = ({ source }) => {
       as="video"
       poster="/no-signal.jpg"
     >
-      <Box as="source" src={source} />
+      <source
+        src="https://res.cloudinary.com/dmtf1daqp/video/upload/q_auto/v1613684508/last-test_jwynpc.mp4"
+        type="video/mp4"
+      />
+      <source
+        src="https://res.cloudinary.com/dmtf1daqp/video/upload/q_auto/v1613684508/last-test_jwynpc.webm"
+        type="video/webm"
+      />
+      <source
+        src="https://res.cloudinary.com/dmtf1daqp/video/upload/q_auto/v1613684508/last-test_jwynpc.ogv"
+        type="video/ogg"
+      />
     </Box>
   );
 };
